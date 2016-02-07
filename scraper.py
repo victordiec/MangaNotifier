02552d6382567d4548866651d1fead896f488205
Robert <robert.otherone@gmail.com>
2016-02-06 23:20:06 -0500

    Added Html parser

--- scraper.py
+++ scraper.py
@@ -1,6 +1,21 @@
-import urllib2
+import HTMLParser
+import urllib
+urlText = []
+class Parsertext(HTMLParser.HTMLParser):
 
-class Scraper:
+    #def __init__(self):
+        #pass
+        def handle_data(self, data):
+            if data != '\n':
+                urlText.append(data)
 
-    def __init__(self):
-        pass
+                #Create instance of HTML parser
+lParser = Parsertext()
+
+thisurl = "http://www-rohan.sdsu.edu/~gawron/index.html"
+#Feed HTML file into parser
+lParser.feed(urllib.urlopen(thisurl).read())
+lParser.close()
+#prints text from page line by line
+for item in urlText:
+    print item
