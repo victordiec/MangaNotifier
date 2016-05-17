abbcee3bc0f213e726efe2683e7c6009b5785b86
Robert <robert.otherone@gmail.com>
2016-05-16 22:15:01 -0400

    Got rest API to launch

--- route.py
+++ route.py
@@ -1,22 +1,8 @@
-from eve import eve
+from eve import Eve
 
 app = Eve()
 
 
 if __name__ == '__main__':
+
     app.run()
-{
-    "_info": {
-        "server": "Eve",
-        "version": "a.b.c",
-        "api_version": "x.y.z"
-    },
-    "_links": {
-        "child": [
-            {
-                "href": "people",
-                "title": "people"
-            }
-        ]
-    }
-}
---
+++ settings.py
@@ -0,0 +1,21 @@
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_USERNAME = 'user'
+MONGO_PASSWORD = 'user'
+MONGO_DBNAME = 'apitest'
+
+DOMAIN = {'manga': {
+    "_info": {
+        "server": "Eve",
+        "version": "a.b.c",
+        "api_version": "x.y.z"
+    },
+    "_links": {
+        "child": [
+            {
+                "href": "people",
+                "title": "people"
+            }
+        ]
+    }
+}}
