25ff6e19a7d58d43ff7d07e37d9b25d8034e8fcd
victordiec <vdiec011@uottawa.ca>
2016-05-29 21:45:46 -0400

    Got it to begin parsing chapters on the manga list

--- HTMLParser.py
+++ HTMLParser.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import sys
+import re
 
 """
 Urls currently being scraped:
@@ -71,7 +73,7 @@ class HTMLParser:
         strong_list = table_list.find_all('strong')
 
         for strong in strong_list[1:]:
-            a = strong.find('a')
+            a = strong.find('a')            
             manga = a.get_text()
             link = a['href']
             manga_list.append({'name': manga, 'link': link})
@@ -83,17 +85,78 @@ class HTMLParser:
     # This is because most methods do some work with the object they're called on,
     # so there needs to be some way for that object to be referred to inside the method.
     # By convention, this first argument is called self inside the method definition:
-    def parse_manga(self, manga_link ):
-        print(manga_link)
+    def parse_manga(self, manga):
+        # print(manga["name"])
+        html = requests.get(manga["link"]).text
+        soup = BeautifulSoup(html, 'html.parser')
+        # print(html)
 
+        table_list = soup.find('table', class_ = 'table table-striped')
+        chapter_list = []        
+
+        td_list = table_list.find_all('td') 
+        a_list = table_list.find_all('a')       
+
+        for a in a_list[1:]:
+            # print(a)
+            chapter = a.get_text()
+            link = a['href']
+            chapter_list.append({'name': chapter, 'link': link})
+
+        return chapter_list
+
+
+
+    def parse_chapters(self, manga_name, chapters):
+        for chapter in chapters:
+            html_parser.parse_chapter(manga_name, chapter)
+            sys.exit()
+
+
+    def parse_chapter(self, manga_name, chapter):
+        print (manga_name, chapter)
+        html = requests.get(chapter["link"]).text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # print(html)
+        dl_list = soup.find_all('ul', class_ = 'dropdown-menu')
+
+        page_list = dl_list[2].find_all('li')
+        last_page = page_list[-1].get_text()
+        # print(last_page)
+
+        match = re.match('Last\s+Page\s+\((\d+)\)', last_page)
+
+        if match:
+            last_page = match.group(1)
+            print("This is the last page", last_page)
+
+            for i in range(1,int(last_page)+1):
+                print(i)
+
+        else:
+            print("Couldn't find the last page")
+
+
+
+
+
+        # print(page_list[-1].get_text())
+
+        sys.exit()
 
 
 if __name__ == '__main__':
     html_parser = HTMLParser()
     master_manga_list = html_parser.parse_mangastream()
 
+    #Grab XML to determine which mangas to download, and which mangas not to download
+
     # print(master_manga_list)
     
     for manga in master_manga_list[1:]:
-        # print(manga)
-        html_parser.parse_manga(manga)
+        # print(manga['name'])
+
+        chapters = html_parser.parse_manga(manga)        
+        html_parser.parse_chapters(manga['name'], chapters)
+
